94edcd76595d3260274c513a6c1b383e83b5c71e
Mirali Fikratzade <miralifikretzade@gmail.com>
2021-05-06 14:51:50 +0200

    commit

---
+++ src/ds/hashtable/HashTableWithProbbing.js
@@ -0,0 +1,149 @@
+import * as Util from "../../util/Util";
+
+const MIN_CAPASITY = 3;
+
+export default class HashTable {
+  constructor() {
+    this.counter = 0;
+    this.INTERNAL_ARRAY_SIZE = MIN_CAPASITY;
+    this.internalArray = [
+      /** INTERNAL_ARRAY_SIZE */
+    ];
+  }
+
+  put(key, value) {
+    if (this.isFull()) {
+      this.reSize();
+    }
+
+    let hash = this.hash(key);
+    let slot = this.internalArray[hash];
+
+    while (slot && slot.key !== key) {
+      slot = this.internalArray[++hash % this.INTERNAL_ARRAY_SIZE];
+    }
+
+    if (slot) {
+      // It means the key already exists inside the hashtable.
+      return undefined;
+    } else {
+      const item = new Item(key, value);
+      this.internalArray[hash % this.INTERNAL_ARRAY_SIZE] = item;
+      this.counter++;
+    }
+  }
+
+  get(key) {
+    const item = this.getItem(key);
+    if (item) {
+      return item.value;
+    }
+    return undefined;
+  }
+
+  set(key, value) {
+    const item = this.getItem(key);
+    if (item) {
+      item.value = value;
+      return item.value;
+    }
+    return undefined;
+  }
+
+  remove(key) {
+    let hash = this.hash(key);
+    let slot = this.internalArray[hash];
+
+    while (slot && slot.key !== key) {
+      slot = this.internalArray[++hash % this.INTERNAL_ARRAY_SIZE];
+    }
+
+    if (slot) {
+      const temp = slot.value;
+      this.internalArray[hash % this.INTERNAL_ARRAY_SIZE] = null;
+      this.counter--;
+      this.reHash();
+      return temp;
+    } else {
+      return undefined;
+    }
+  }
+
+  getItem(key) {
+    let hash = this.hash(key);
+    let slot = this.internalArray[hash];
+
+    while (slot && slot.key !== key) {
+      slot = this.internalArray[++hash % this.INTERNAL_ARRAY_SIZE];
+    }
+
+    return slot;
+  }
+
+  hash(key) {
+    return key.length % this.INTERNAL_ARRAY_SIZE;
+  }
+
+  size() {
+    return this.counter;
+  }
+
+  reHash() {
+    const copyOfInternalArray = this.internalArray;
+    this.internalArray = [
+      /** this.INTERNAL_ARRAY_SIZE */
+    ];
+    this.counter = 0;
+
+    for (let i = 0; i < this.INTERNAL_ARRAY_SIZE; i++) {
+      const item = copyOfInternalArray[i];
+      if (item) {
+        this.put(item.key, item.value);
+      }
+    }
+  }
+
+  reSize() {
+    const copyOfInternalArray = this.internalArray;
+    const copyOfSize = this.INTERNAL_ARRAY_SIZE;
+    this.INTERNAL_ARRAY_SIZE *= 2;
+    this.internalArray = [
+      /** this.INTERNAL_ARRAY_SIZE */
+    ];
+    this.counter = 0;
+
+    for (let i = 0; i < copyOfSize; i++) {
+      const item = copyOfInternalArray[i];
+      if (item) {
+        this.put(item.key, item.value);
+      }
+    }
+  }
+
+  isFull() {
+    return this.size() === this.INTERNAL_ARRAY_SIZE;
+  }
+
+  isEmpty() {
+    return this.size() === 0;
+  }
+
+  print() {
+    for (let i = 0; i < this.INTERNAL_ARRAY_SIZE; i++) {
+      if (this.internalArray[i]) {
+        Util.print(
+          `${i} = (${this.internalArray[i].key} => ${this.internalArray[i].value})`
+        );
+      } else {
+        Util.print(`${i} = (NULL)`);
+      }
+    }
+  }
+}
+
+class Item {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import * as Util from "./util/Util";
 
-import HashTable from "./ds/hashtable/HashTableWithChaining";
+//import HashTable from "./ds/hashtable/HashTableWithChaining";
+import HashTable from "./ds/hashtable/HashTableWithProbbing";
 
 const hashTable = new HashTable();
-hashTable.put("bir", 1);
-hashTable.put("iki", 2);
-hashTable.put("uc", 3);
-hashTable.put("dort", 4);
-hashTable.put("bes", 5);
-hashTable.put("alti", 6);
+// hashTable.put("bir", 1);
+// hashTable.put("iki", 2);
+// hashTable.put("uc", 3);
+// hashTable.put("dort", 4);
+// hashTable.put("bes", 5);
+// hashTable.put("alti", 6);
 
-hashTable.put("", 666);
+//hashTable.put("", 666);
 
-hashTable.put("alti", 7);
+//hashTable.put("alti", 7);
 
 //hashTable.print();
 
@@ -20,12 +21,16 @@ hashTable.put("alti", 7);
 // Util.print(hashTable.get("alal"));
 // Util.print(hashTable.get(""));
 
-Util.print(hashTable.set("alti", 66));
+// Util.print(hashTable.set("alti", 66));
 
-Util.print(hashTable.set("eded", 77));
+// Util.print(hashTable.set("eded", 77));
 
-Util.print(hashTable.remove("alti"));
+//hashTable.print();
 
-Util.print(hashTable.remove("dort"));
+//Util.print(hashTable.remove("alti"));
 
-hashTable.print();
+//Util.print("alti removed");
+
+// Util.print(hashTable.remove("dort"));
+
+//hashTable.print();
